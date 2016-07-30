30b5b2da02dfb01e89d17194d25ad4b454bb736f
Rahul Alexander <alexander@chroma.org>
2016-07-30 15:55:04 -0400

    Rails Setup

---
+++ app/controllers/jokes_controller.rb
@@ -0,0 +1,11 @@
+class JokesController < ApplicationController
+
+  def index
+    url="http://api.icndb.com/jokes/random/5"
+    response = HTTParty.get(url)
+    parsed_body = JSON.parse(response.body)
+    render json: parsed_body
+
+  end
+
+end
