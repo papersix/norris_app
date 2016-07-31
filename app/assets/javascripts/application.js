8ff4886fbc066e5e81dd760b1b4de61aa68023aa
Rahul Alexander <alexander@chroma.org>
2016-07-31 10:31:56 -0400

    showing 1 random  result from API

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,3 +13,49 @@
 //= require jquery
 //= require jquery_ujs
 //= require_tree .
+
+
+function renderJoke (oneJoke) {
+console.log(oneJoke.joke)
+  let $sampleJoke = oneJoke.joke
+  let $joke = $('<h3>').text($sampleJoke)
+  var $container = $('#joke-container')
+  // var $joke = $('<li class="joke">' + oneJoke.joke + '</li>')
+
+  // $joke.appendTo($('body'))
+  $container.append($joke)
+
+}
+
+function getJokes(){
+  $.getJSON('/jokes').done(function(jokes) {
+    let oneJoke = jokes.value.shift()
+    // console.log(oneJoke)
+    // oneJoke.forEach(function(joke){
+
+      renderJoke(oneJoke)
+
+    // })
+  })
+}
+
+$(function() {
+  getJokes();
+});
+
+
+
+// function getJokes(){
+//   $.getJSON('/jokes').done(function(jokes) {
+//     let oneJoke = jokes.value.shift()
+//     console.log(jokes.value)
+//     jokes.results.forEach(function(joke){
+//       renderJoke(joke);
+//     })
+//   })
+// }
+
+
+
+
+
